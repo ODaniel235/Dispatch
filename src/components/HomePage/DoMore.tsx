18f5ac1e89dbe38690f75e622c3ca2312115afdc
Oladele-israel <omoleoladele43@gmail.com>
2024-12-04 21:37:43 +0100

    new  push

---
+++ src/components/HomePage/DoMore.tsx
@@ -0,0 +1,25 @@
+const DoMore = () => {
+  return (
+    <section className="py-10 bg-[#fff6f3] mt-28 px-10 mx-auto">
+      <h1>Do More with our Mobile App</h1>
+      <div>
+        <div>
+          <h1>
+            Delivery your Logistics Safely and
+            <span className="text-orange">Quickly</span>
+          </h1>
+          <p>
+            We blend fast, bike-based delivery with the convenience of our
+            mobile app. Our team of cyclists ensures quick, reliable service
+            while you track and manage your orders effortlessly through our app.
+            Committed to speed and sustainability, we’re here to transform your
+            local delivery experience.
+          </p>
+        </div>
+        <div></div>
+      </div>
+    </section>
+  );
+};
+
+export default DoMore;
--- src/constants/Herocontants.ts
+++ src/constants/Herocontants.ts
@@ -1,23 +1,31 @@
-export const heroContent = {
+import boxtime from "../../public/box-time.svg";
+import location from "../../public/location.svg";
+import convert3D from "../../public/convert-3d-cube.svg";
+
+const heroContent = {
   text: "unlock seamless deliveries Now!",
   subtext:
     "Enjoy fast, reliable and eco-friendly delivery solutions that keep your business moving efficiently",
 };
 
-export const services = [
+const services = [
   {
     heading: "Local Delivery",
     subHeading: "Need to send a package across town ?",
     desc: "Need it there fast? Our same-day delivery option ensures your parcel gets to its destination within hours.",
+    icon: location,
   },
   {
     heading: "Scheduled Deliveries",
     subHeading: "Need to send a package across town ?",
     desc: "Plan your deliveries in advance and let us handle the timing. Perfect for businesses needing regular dispatch services.",
+    icon: boxtime,
   },
   {
     heading: "Bulk Deliveries",
     subHeading: "Need to send a package across town ?",
     desc: "We manage bulk shipments with ease, ensuring your larger consignments are delivered efficiently and safely..",
+    icon: convert3D,
   },
 ];
+export { heroContent, services };
