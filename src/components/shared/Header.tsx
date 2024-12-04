71836789276584bb55d14ea95841d37d55ddaf95
Omole Oluwadamilare Daniel <domole235@gmail.com>
2024-12-04 22:13:19 +0100

    sec

--- src/components/shared/Header.tsx
+++ src/components/shared/Header.tsx
@@ -3,105 +3,6 @@ import { useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import Logo from "./Logo";
 
-function Menubar({ onClick }: { onClick?: any }) {
-  /*  const pathName = usePathname(); */
-  const [menuShow, setMenuShow] = useState(false);
-  const location = useLocation();
-  const pathName = location.pathname;
-  function handleChange() {
-    if (!menuShow) {
-      setMenuShow(true);
-    } else {
-      setMenuShow(false);
-    }
-  }
-
-  return (
-    <div className="flex flex-col relative w-fit sm:hidden">
-      <svg
-        onClick={handleChange}
-        xmlns="http://www.w3.org/2000/svg"
-        width="38"
-        height="38"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        className="lucide lucide-align-right sm:hidden cursor-pointer"
-      >
-        <line x1="21" x2="3" y1="6" y2="6" />
-        <line x1="21" x2="9" y1="12" y2="12" />
-        <line x1="21" x2="7" y1="18" y2="18" />
-      </svg>
-      <div
-        className={
-          !menuShow
-            ? "flex flex-col absolute top-20 right-0 z-20 bg-white text-right w-40 opacity-0 pointer-events-none transition-all"
-            : "flex flex-col absolute top-20 right-0 z-20 bg-white text-right w-44 opacity-100 transition-all"
-        }
-      >
-        <ul className="flex flex-col p-5 gap-10 ">
-          <Link to="/about" className="mr-1">
-            <span
-              onClick={handleChange}
-              className={
-                pathName === "/about"
-                  ? "bg-[#FE581C] text-white p-1 rounded-full"
-                  : ""
-              }
-            >
-              About Us
-            </span>
-          </Link>
-          <Link to="/track" className="mr-1">
-            <span
-              onClick={handleChange}
-              className={
-                pathName === "/track"
-                  ? "bg-[#FE581C] text-white p-1 rounded-full"
-                  : ""
-              }
-            >
-              Track Delivery
-            </span>
-          </Link>
-          <Link to="/ourService" className="mr-1">
-            <span
-              onClick={handleChange}
-              className={
-                pathName === "/ourService"
-                  ? "bg-[#FE581C] text-white p-1 rounded-full"
-                  : ""
-              }
-            >
-              Our Service
-            </span>
-          </Link>
-          <Link to="/contact" className="mr-1">
-            <span
-              onClick={handleChange}
-              className={
-                pathName === "/contact"
-                  ? "bg-[#FE581C] text-white p-1 rounded-full"
-                  : ""
-              }
-            >
-              Contact Us
-            </span>
-          </Link>
-          <Link to={"/register"}>
-            <button className="  bg-orange text-white font-unaego-light text-xl w-fit h-fit px-4 flex justify-center items-center rounded-full hover:scale-110">
-              Register
-            </button>
-          </Link>
-        </ul>
-      </div>
-    </div>
-  );
-}
-
 const Header = () => {
   const [isActive, setIsActive] = useState<number | null>(null);
   const [hamburgerOpen, setHamburgerOpen] = useState<boolean>(false);
@@ -111,7 +12,7 @@ const Header = () => {
     return value;
   };
   return (
-    <header className=" w-screen h-fit flex flex-row gap-4 fixed  px-2 lg:px-8 py-4 justify-between md:items-center bg-white opacity-100 bg-opacity-100 z-10 shadow-xl">
+    <header className="w-screen h-fit flex flex-row gap-4 px-2 lg:px-8 py-2 md:py-4 justify-between md:items-center bg-white opacity-100 bg-opacity-100 drop-shadow-lg sticky top-0 z-50">
       {/* Desktop screen */}
       <Logo />
       <nav className=" hidden md:flex">
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,8 +1,9 @@
+import Blocks from "@/components/blocks";
 import OrangeStar from "@/components/OrangeStar";
 
-const About = () => {
+function page() {
   return (
-    <div className="flex flex-col justify-center items-center py-14 max-md:py-14 gap-20 px-8 max-sm:text-center">
+    <div className="flex flex-col justify-center items-center py-14 max-md:py-14 gap-20 px-8 max-sm:text-center overflow-x-hidden">
       <div className="flex flex-col w-full max-w-[1240px] gap-[64px] py-[40px] max-md:py-0 relative justify-between">
         <div className="max-sm:hidden">
           <OrangeStar size={56} top={160} right={0} />
@@ -10,10 +11,10 @@ const About = () => {
           <OrangeStar size={56} top={100} left={10} />
         </div>
         <div className="flex flex-col justify-center items-center text-center gap-6">
-          <h1 className="text-[#1E1E1E] font-semibold text-center text-5xl max-sm:text-[32px] font-custom">
+          <h1 className="text-[#1E1E1E] font-semibold text-center text-5xl max-sm:text-[32px] font-unaego">
             About Dispatch Hub
           </h1>
-          <p className="font-customLight max-w-[1027px] text-center">
+          <p className="font-unaego-light text-center">
             At Dispatch Hub, we’re all about making deliveries easy and
             reliable. As a leading logistics company in Nigeria, we take pride
             in offering fast, secure, and affordable delivery services across
@@ -31,13 +32,106 @@ const About = () => {
             className="max-sm:w-[170px] max-sm:h-[223px]"
             width={500}
             height={580.26}
-            src="./dispatchRider.png"
+            src={"./dispatchRider.png"}
             alt="dispatch_rider"
           />
         </div>
       </div>
+      <div className="flex max-w-[1240px] w-full">
+        <div
+          className={
+            "flex flex-row w-full justify-between gap-6 max-lg:flex-col"
+          }
+        >
+          <div className="flex flex-col items-start w-full gap-6">
+            <h2 className="text-[#1E1E1E] w-full font-semibold text-4xl max-sm:text-2xl font-unaego">
+              Impact
+            </h2>
+            <p className="text-lg max-sm:text-sm font-unaego-light">
+              At Dispatch Hub, our focus goes beyond just delivering packages;
+              we aim to create lasting connections that empower businesses and
+              individuals alike. We cater to the growing needs of small
+              businesses, helping them reach new customers and expand their
+              horizons across Nigeria. By offering fast, reliable, and
+              cost-effective delivery services, we play a key role in fueling
+              local economies and supporting the growth of the digital
+              marketplace. Our team is committed to providing innovative
+              logistics solutions that enhance customer experiences and
+              streamline operations. With every delivery, we strive to
+              contribute to a more connected, efficient, and prosperous future
+              for Nigeria.
+            </p>
+          </div>
+        </div>
+      </div>
+      <Blocks
+        title="Our Mission"
+        desc="At Dispatch Hub, we aim to revolutionize delivery services in Nigeria by offering innovative, seamless, and accessible logistics solutions. We strive to support the growth of businesses, online platforms, and individuals by ensuring timely and secure transportation of goods. Our commitment is to provide a hassle-free delivery experience that customers can rely on, making us a go-to partner for efficient and trustworthy courier services nationwide. Through our dedication to reliability and excellence, we are setting a new standard in Nigeria's logistics industry."
+        imgSrc={"./ourVision.png"}
+        imgAlt="localDelivery"
+      />
+      <Blocks
+        title="Our Vision"
+        desc="At Dispatch Hub, our vision is to become the leading force in transforming Nigeria's logistics industry through innovation, efficiency, and customer-centric solutions. We aspire to build a future where every business, regardless of size, has access to seamless delivery services that connect them to their customers effortlessly. By leveraging technology and sustainable practices, we aim to set new benchmarks in reliability and speed, fostering stronger connections across Nigeria. Our goal is to empower businesses and individuals, helping them thrive in an increasingly digital and interconnected world, while driving the evolution of logistics in the country"
+        imgSrc={"./ourMission.png"}
+        imgAlt="scheduledDeliveries"
+        reversed={true}
+      />
+      <div className="flex max-w-[1240px] relative w-full">
+        <div className="max-sm:hidden">
+          <OrangeStar size={32} left={50} color="#021336" />
+        </div>
+        <div
+          className={
+            "flex flex-row items-center relative w-full justify-between gap-6 max-lg:flex-col"
+          }
+        >
+          <div className="flex flex-col max-w-[564px] w-full gap-10 max-sm:text-center">
+            <div className="flex flex-col items-start w-full gap-6">
+              <h2 className="text-[#1E1E1E] w-full font-semibold text-4xl max-sm:text-2xl font-custom">
+                Download Our App <span className="text-[#F3693B]">Today!</span>
+              </h2>
+              <p className="text-lg max-sm:text-sm font-unaego-light">
+                Enhance your dispatch experience with our easy-to-use mobile
+                app. Whether you’re managing deliveries, tracking your orders,
+                or booking dispatch services, our app provides a seamless and
+                efficient solution right at your fingertips.
+              </p>
+            </div>
+            <div className="flex flex-col md:flex-row gap-6">
+              <img
+                className="max-sm:w-full rounded-[20px] max-sm:rounded-[5.4px]"
+                width={100}
+                src={"./AppsStore.png"}
+                alt="App"
+              />
+              <img
+                className="max-sm:w-full rounded-[20px] max-sm:rounded-[5.4px]"
+                width={100}
+                src={"./PlayStore.png"}
+                alt="App"
+              />
+            </div>
+          </div>
+          <div className="flex flex-row relative justify-center items-center w-full max-w-[567.42px]">
+            <div className="max-sm:hidden">
+              <OrangeStar size={32} left={30} />
+              <OrangeStar size={56} bottom={100} left={60} color="#021336" />
+            </div>
+            <div className="hidden max-sm:flex">
+              <OrangeStar size={33} bottom={0} left={20} color="#021336" />
+            </div>
+            <img
+              className="max-sm:w-[160.8px]"
+              width={272}
+              src={"./app.png"}
+              alt="App"
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
-};
+}
 
-export default About;
+export default page;
