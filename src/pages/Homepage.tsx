8046f796645139e86b0046650f8211874e0c82c5
Oladele-israel <omoleoladele43@gmail.com>
2024-12-04 01:33:42 +0100

    upodated hero

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import Hero from "@/components/HomePage/Hero";
+import OurServices from "@/components/HomePage/OurServices";
 
 const Homepage = () => {
   return (
     <div>
-      
+      <Hero />
+      <OurServices />
     </div>
-  )
-}
+  );
+};
 
-export default Homepage
+export default Homepage;
